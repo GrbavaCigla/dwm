32e349c562b626c475bfbac40af80a8066848da9
GrbavaCigla <alexa.ognjanovic@protonmail.com>
2020-11-06 22:28:18 +0100

    Config -> Config.def

--- config.def.h
+++ config.def.h
@@ -8,28 +8,24 @@ static const unsigned int gappx     = 18;       /* gap pixel between windows */
 static const unsigned int snap      = 16;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int vertpad            = 10;       /* vertical padding of bar */
-static const int sidepad            = 10;       /* horizontal padding of bar */
+static const int vertpad            = 18;       /* vertical padding of bar */
+static const int sidepad            = 18;       /* horizontal padding of bar */
 static const int horizpadbar        = 12;        /* horizontal padding for statusbar */
 static const int vertpadbar         = 12;        /* vertical padding for statusbar */
 static const char *fonts[]          = {"Iosevka:size=12", "Font Awesome 5 Free Solid:size=12" };
 
-static const char norm_fg[] = "#d0c9be";
-static const char norm_bg[] = "#18140d";
-static const char norm_border[] = "#918c85";
+static char normfgcolor[] = "#d0c9be";
+static char normbgcolor[] = "#18140d";
+static char normbordercolor[] = "#918c85";
 
-static const char sel_fg[] = "#d0c9be";
-static const char sel_bg[] = "#AAB234";
-static const char sel_border[] = "#d0c9be";
+static char selfgcolor[] = "#d0c9be";
+static char selbgcolor[] = "#AAB234";
+static char selbordercolor[] = "#d0c9be";
 
-static const char urg_fg[] = "#d0c9be";
-static const char urg_bg[] = "#929F25";
-static const char urg_border[] = "#929F25";
-
-static const char *colors[][3]      = {
+static char *colors[][3]      = {
     /*               fg           bg         border                         */
-    [SchemeNorm] = { norm_fg,     norm_bg,   norm_border }, // unfocused wins
-    [SchemeSel]  = { sel_fg,      sel_bg,    sel_border },  // the focused win
+    [SchemeNorm] = { normfgcolor,     normbgcolor,   normbordercolor }, // unfocused wins
+    [SchemeSel]  = { selfgcolor,      selbgcolor,    selbordercolor },  // the focused win
 //    [SchemeUrg] =  { urg_fg,      urg_bg,    urg_border },
 };
 
@@ -72,6 +68,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", NULL };
+static const char *dmenuapps[] = { "dmenu_apps", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 static const char *uslang[] = {"/home/cigla/.bin/keyboard.sh", "us", NULL};
@@ -85,10 +82,13 @@ static const char *mutevol[] = { "/home/cigla/.bin/volmute.sh",       NULL };
 static const char *upbright[] = { "/home/cigla/.bin/brightup.sh",     NULL };
 static const char *downbright[] = { "/home/cigla/.bin/brightdown.sh", NULL };
 
+static const char *screenshot[] = { "/home/cigla/.bin/screenshot.sh", NULL };
+
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_a,      spawn,          {.v = dmenuapps } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_F1,     spawn,          {.v = uslang } },
 	{ MODKEY|ShiftMask,             XK_F2,     spawn,          {.v = rslang } },
@@ -99,6 +99,7 @@ static Key keys[] = {
 	{ 0,              XF86XK_AudioRaiseVolume, spawn,          {.v = upvol } },
 	{ 0,               XF86XK_MonBrightnessUp, spawn,          {.v = upbright } },
 	{ 0,             XF86XK_MonBrightnessDown, spawn,          {.v = downbright } },
+	{ 0,                            0xff61,    spawn,          {.v = screenshot } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -125,6 +126,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	// { MODKEY,                       XK_F5,     xrdb,           {.v = NULL } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
